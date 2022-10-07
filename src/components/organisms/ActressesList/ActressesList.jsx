76ab8b849afd32dd29f3b3379a3c6aebf0017f55
mirkoperamas <mirkoperamas@gmail.com>
2022-10-07 04:42:37 -0500

    update

--- src/components/organisms/ActressesList/ActressesList.jsx
+++ src/components/organisms/ActressesList/ActressesList.jsx
@@ -7,17 +7,24 @@ export const ActressesList = ({ data }) => {
         <h1>Lista de Actrices</h1>
 
         <div className={classes.list__actresses}>
-          {data.actresses.map((actress) => {
-            return (
-              <div className={classes.list__actressesCard} key={actress?.id}>
-                <div>
-                  <img src={actress?.img} alt={`actress-${actress?.id}`} />
-                </div>
+          {data && (
+            <>
+              {data.actresses.map((actress) => {
+                return (
+                  <div
+                    className={classes.list__actressesCard}
+                    key={actress?.id}
+                  >
+                    <div>
+                      <img src={actress?.img} alt={`actress-${actress?.id}`} />
+                    </div>
 
-                <div>{actress?.name}</div>
-              </div>
-            );
-          })}
+                    <div>{actress?.name}</div>
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
       </div>
     </section>
